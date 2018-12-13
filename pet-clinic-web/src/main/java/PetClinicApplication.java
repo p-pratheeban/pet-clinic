6201544d9763d045e4602779fa103ae644fd83ae
unknown <pratheeban.panch@gmail.com>
2018-12-13 02:57:11 -0500

    Refactoring Muti Module Projext. Closes #2.

---
+++ pet-clinic-web/src/main/java/PetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.pratheeban;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicApplication 
+{
+    public static void main( String[] args )
+    {
+    	PetClinicApplication.run(PetClinicApplication.class,args);
+    }
+}
