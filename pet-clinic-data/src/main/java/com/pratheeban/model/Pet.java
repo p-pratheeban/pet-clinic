fbcae367c335ca96d04763fcd007935b8c3b1087
unknown <pratheeban.panch@gmail.com>
2018-12-13 23:02:05 -0500

    Adding BaseEntity

--- pet-clinic-data/src/main/java/com/pratheeban/model/Pet.java
+++ pet-clinic-data/src/main/java/com/pratheeban/model/Pet.java
@@ -2,7 +2,7 @@ package com.pratheeban.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 	private PetType petType;
 	private Owner owner;
 	private LocalDate birthDate;
