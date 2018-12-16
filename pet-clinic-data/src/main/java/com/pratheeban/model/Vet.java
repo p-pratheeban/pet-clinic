f9a087ad928b783082a89eb31da78446dc22b3a5
unknown <pratheeban.panch@gmail.com>
2018-12-15 22:18:03 -0500

    Convert Model into JPA Entity.

--- pet-clinic-data/src/main/java/com/pratheeban/model/Vet.java
+++ pet-clinic-data/src/main/java/com/pratheeban/model/Vet.java
@@ -1,10 +1,22 @@
 package com.pratheeban.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
-	private Set<Speciality> specialities;
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
+	private Set<Speciality> specialities = new HashSet<>();
 
 	public Set<Speciality> getSpecialities() {
 		return specialities;
